f7d06b11599b20373144404c68f14f6b5564d5f9
Ryuusyr <shnitailan65@gmail.com>
2021-11-04 13:46:49 +0300

    breakout

--- population/population.py
+++ population/population.py
@@ -2,16 +2,18 @@ from numpy import *
 
 population = 2
 a = 1
-
+k = 1
 def formula(i, pop):
-    R_factor = random.uniform(1.5,4)
-    while pop < 10000:
+    R_factor = random.uniform(1.25,1.883)
+    while pop < 100000:
+        temppop = pop
         pop = pop * R_factor
         pop = round(pop)
         i += 1
-        if i % 10 == 0 and pop < 10000:
-            print(f"test {i}: {pop}, r: {R_factor}")
-    print(f"{i}: {pop}, r: {R_factor}")
+        if temppop == pop:
+            break
+    return i,pop,R_factor
 
 for i in range(40):
-    formula(a,population)
+    print(k, ":", formula(a,population))
+    k += 1
