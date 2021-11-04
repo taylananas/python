8a4bed9be6e83462c29d5d1460574f0d7fc9999c
Ryuusyr <shnitailan65@gmail.com>
2021-11-05 02:06:32 +0300

    eur

--- rate/ratecalc.py
+++ rate/ratecalc.py
@@ -1,13 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.bloomberght.com/doviz/dolar"
+def usdrate():
+    urlusd = "https://www.bloomberght.com/doviz/dolar"
+    r_usd = requests.get(urlusd)
+    soup = BeautifulSoup(r_usd.content, "html.parser")
+    selection = soup.find("span", {"class": "downRed"})
+    selectionrate = soup.find("span", {"class": "bulk"})
+    selectiondate = soup.find("span", {"class": "date"})
+    print("USD/TRY: ",selection.text," Rate:",selectionrate.text, "Date: ", selectiondate.text)
 
-r = requests.get(url)
+def eurrate():
+    urlusd = "https://www.bloomberght.com/doviz/euro"
+    r_usd = requests.get(urlusd)
+    soup = BeautifulSoup(r_usd.content, "html.parser")
+    selection = soup.find("span", {"class": "downRed"})
+    selectionrate = soup.find("span", {"class": "bulk"})
+    selectiondate = soup.find("span", {"class": "date"})
+    print("EUR/TRY: ",selection.text," Rate:",selectionrate.text, "Date: ", selectiondate.text)   
 
-soup = BeautifulSoup(r.content, "html.parser")
-
-selection = soup.find("span", {"class": "downRed"})
-selectionrate = soup.find("span", {"class": "bulk"})
-selectiondate = soup.find("span", {"class": "date"})
-print("USD/TRY: ",selection.text," Rate:",selectionrate.text, "Date: ", selectiondate.text)
+usdrate()
+eurrate()
