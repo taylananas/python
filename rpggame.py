5375d22f1eb1251e520cf5c96c1f8882b7809045
Ryuusyr <shnitailan65@gmail.com>
2021-11-04 13:04:55 +0300

    nce

--- rpggame.py
+++ rpggame.py
@@ -4,6 +4,8 @@ from tkinter import *
 def main():
     MainWindow = Tk()
     health = IntVar()
+    level = IntVar()
+    level.set(1)
     health.set(100)
     healthlabel = Label(MainWindow,text = "health:").grid(row=0,column=0)
     healthentry = Entry(MainWindow,textvariable=health,state="disabled",width=3).grid(row=0,column=1)
