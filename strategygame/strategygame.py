476d904d6fb4b3fc36218174468280b38349f300
Ryuusyr <shnitailan65@gmail.com>
2021-11-05 13:07:49 +0300

    button def

--- strategygame/strategygame.py
+++ strategygame/strategygame.py
@@ -3,10 +3,23 @@ from tkinter import *
 main=Tk()
 
 main.geometry("600x400")
-sp = Button(main,text="Singleplayer",width=12,height=5)
-mp = Button(main,text="Multiplayer",width=12,height=5)
+
+def single():
+    main.destroy()
+    singleplayer = Tk()
+
+    singleplayer.mainloop()
+
+def multi():
+    main.destroy()
+    multiplayer = Tk()
+
+    multiplayer.mainloop()
+
+sp = Button(main,text="Singleplayer",width=12,height=5,command=single)
+mp = Button(main,text="Multiplayer",width=12,height=5,command=multi)
+
 sp.place(relx=0.5,rely=0.3,anchor=CENTER)
 mp.place(relx=0.5,rely=0.7,anchor=CENTER)
-
 
 main.mainloop()
