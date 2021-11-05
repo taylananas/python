52665ae295946b607ff3033304612614509b18ce
Ryuusyr <shnitailan65@gmail.com>
2021-11-05 13:55:22 +0300

    buy menu

--- strategygame/strategygame.py
+++ strategygame/strategygame.py
@@ -1,7 +1,7 @@
 from tkinter import *
+from numpy import random as r
 
 main=Tk()
-
 main.geometry("600x400")
 
 def single():
@@ -18,12 +18,16 @@ def single():
     moneye.grid(row=1,column=1)
     Label().grid(row=2)
     #army
+    moneyint.set(1000)
     infamount = StringVar()
     cavamount = StringVar()
     artamount = StringVar()
+    infamount.set(0)
+    cavamount.set(0)
+    artamount.set(0)
     armyl = Label(singleplayer,text="Army Overview")
     armyl.grid(row=3,column=1)
-    infantry = Label(singleplayer,text="Infantry:")
+    infantry = Label(singleplayer,text="Infantry")
     infantry.grid(row=4,column=0)
     infantrye = Entry(singleplayer,state="disabled",textvariable=infamount,width=8)
     infantrye.grid(row=4,column=1)
@@ -35,6 +39,27 @@ def single():
     artillery.grid(row=6,column=0)
     artillere = Entry(singleplayer,state="disabled",textvariable=artamount,width=8)
     artillere.grid(row=6,column=1)
+
+    def infbuy():
+        if int(moneyint.get()) >= 100:
+            infamount.set(str(int(infamount.get())+10))
+            moneyint.set(str(int(moneyint.get())-100))
+    def cavbuy():
+        if int(moneyint.get()) >= 150:
+            cavamount.set(str(int(cavamount.get())+5))
+            moneyint.set(str(int(moneyint.get())-150))            
+    def artbuy():
+        if int(moneyint.get()) >= 300:
+            artamount.set(str(int(artamount.get())+3))
+            moneyint.set(str(int(moneyint.get())-300))
+
+
+    infbuybut = Button(singleplayer,text="Buy 10x Infantry\nCost 100",command=infbuy)
+    infbuybut.grid(row=4,column=3)
+    cavbuybut = Button(singleplayer,text="Buy 5x Cavalry\nCost 150",command=cavbuy)
+    cavbuybut.grid(row=5,column=3)
+    artbuybut = Button(singleplayer,text="Buy 3x Artillery\nCost 300",command=artbuy)
+    artbuybut.grid(row=6,column=3)
 
     singleplayer.mainloop()
 
