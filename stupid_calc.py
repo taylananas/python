f825b67a8b37a35ab64f5e307608b65304c96a4b
Ryuusyr <shnitailan65@gmail.com>
2021-11-07 02:59:57 +0300

    tkinter edit

--- stupid_calc.py
+++ stupid_calc.py
@@ -18,9 +18,33 @@ main = Tk()
 
 def p_passbut():
     main.destroy()
-    p_passtk = Tk()
 
+    p_passtk = Tk()
+    weight = IntVar()
+    height = IntVar()
+    footsize = IntVar()
+    p_lenght = IntVar()
+
+    def p_formula():
+        intweight = weight.get()
+        intheight = height.get()
+        intfoot = footsize.get()
+        answer = intweight*intfoot/intheight
+        answer = round(answer,2)
+        p_lenght.set(answer)
+
+    Label(p_passtk,text="Weight(kg):").grid(row=0,column=0)
+    Entry(p_passtk,textvariable=weight,width=6).grid(row=0,column=1)
+    Label(p_passtk,text="Height (cm)").grid(row=1,column=0)
+    Entry(p_passtk,textvariable=height,width=6).grid(row=1,column=1)
+    Label(p_passtk,text="Foot Size(eu)").grid(row=2,column=0)
+    Entry(p_passtk,textvariable=footsize,width=6).grid(row=2,column=1)
+    Button(p_passtk,text="Calculate ur p size",command=p_formula).grid(row=3,column=0,columnspan=2)
+    Label(p_passtk,text="P Length(cm)").grid(row=4,column=0)
+    Entry(p_passtk,textvariable=p_lenght,width=6,state="disabled").grid(row=4,column=1)
     p_passtk.mainloop()
+
 p_passbutton = Button(main,text="P_Pass",command=p_passbut).grid(row=0,column=0)
+
 
 main.mainloop()
