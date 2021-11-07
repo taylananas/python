c7f72197d517afac2088e03963ef01889d76a0df
Ryuusyr <shnitailan65@gmail.com>
2021-11-07 06:18:15 +0300

    random finish

--- randomimageselector.py
+++ randomimageselector.py
@@ -1,14 +1,49 @@
 from tkinter import *
 from tkinter import filedialog
+from PIL import Image,ImageTk
+import os
+import random as r
 
 def main():
     mainwindow = Tk()
-    
+    global selected_folder
+    selected_folder = ""
     def fileselection():
+        global selected_folder
         selected_folder = filedialog.askdirectory()
 
-    frame1 = LabelFrame(mainwindow,padx=20,pady=20).grid(row=0,column=0)
-    Button(frame1,text="Select Directory",command=fileselection).grid(row=0,column=0)
+    frame1 = LabelFrame(mainwindow,padx=20,pady=20)
+    frame1.grid(row=0,column=0)
+    bt1 = Button(frame1,text="Select Directory",command=fileselection)
+    bt1.grid(row=0,column=0)
+    frame2 = LabelFrame(mainwindow,padx=20,pady=20)
+    frame2.grid(row=0,column=1)
+
+    def directorylisting(path):
+        global listfiles
+        listfiles = os.listdir(path)
+        global filecount
+        filecount = len(listfiles)
+        randomnum = r.randint(0,filecount-1)
+        randomimage = listfiles[randomnum]
+        global image1
+        global imagelabel
+        imagelabel = Label()
+        try:
+            imagelabel.grid_forget()
+            image1 = ImageTk.PhotoImage(Image.open(path+"/"+randomimage).resize([800,600]))
+            imagelabel = Label(frame2,image=image1)
+            imagelabel.grid(row=0,column=0)        
+        except:
+            image1 = ImageTk.PhotoImage(Image.open(path+"/"+randomimage).resize([800,600]))
+            imagelabel = Label(frame2,image=image1)
+            imagelabel.grid(row=0,column=0)
+
+
+
+
+    bt2 = Button(frame1,command=lambda:directorylisting(selected_folder),text="Random Image")
+    bt2.grid(row=1,column=0)
     
     mainwindow.mainloop()
 
