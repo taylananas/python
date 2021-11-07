9d658851d258bcb8df5a4d412b6038ca348f1496
Ryuusyr <shnitailan65@gmail.com>
2021-11-07 03:01:21 +0300

    edit

--- stupid_calc.py
+++ stupid_calc.py
@@ -1,17 +1,3 @@
-# (kilo*ayak numarası)/boy(cm)
-
-# def formula(kg,size,cm):
-#     p_pass = kg*size/cm
-#     return p_pass
-
-# a = int(input("Kilo:"))
-# b = int(input("Ayak Numarası:"))
-# c = int(input("Boyunuz (cm):"))
-
-# answer = formula(a,b,c)
-# answer = round(answer,2)
-# print(answer)
-
 from tkinter import *
 
 main = Tk()
