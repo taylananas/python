708a3b18d9d3cc2e4ac6a9007edbf67548a1a85d
Ryuusyr <shnitailan65@gmail.com>
2021-11-07 03:20:04 +0300

    final

---
+++ euler9.py
@@ -0,0 +1,17 @@
+# a + b + c = 1000
+# a^2 + b^2 = c^2
+# a < b < c
+# a.b.c = ?
+
+def main():
+    for c in range(1,1000):
+        for b in range(1,1000-c):
+            a = 1000 - (c + b)
+            if a**2 + b**2 == c**2 and a<b<c:
+                print(a,b,c)
+                print(a*b*c)
+            
+
+
+if __name__ == '__main__':
+    main()
