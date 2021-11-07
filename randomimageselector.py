50a9cb61135a15cf3e5d5c9f55006ff9cebdbcf2
Ryuusyr <shnitailan65@gmail.com>
2021-11-07 05:40:10 +0300

    init

---
+++ randomimageselector.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import filedialog
+
+def main():
+    mainwindow = Tk()
+    
+    def fileselection():
+        selected_folder = filedialog.askdirectory()
+
+    frame1 = LabelFrame(mainwindow).grid(row=0,column=0)
+    Button(frame1,text="Select Directory",command=fileselection).grid(row=0,column=0)
+    
+    mainwindow.mainloop()
+
+if __name__ == '__main__':
+    main() 
