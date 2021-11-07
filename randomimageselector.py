3ec2b0edeea2fb2890bf05cf7245418060cea878
Ryuusyr <shnitailan65@gmail.com>
2021-11-07 05:41:12 +0300

    padx pady

--- randomimageselector.py
+++ randomimageselector.py
@@ -7,7 +7,7 @@ def main():
     def fileselection():
         selected_folder = filedialog.askdirectory()
 
-    frame1 = LabelFrame(mainwindow).grid(row=0,column=0)
+    frame1 = LabelFrame(mainwindow,padx=20,pady=20).grid(row=0,column=0)
     Button(frame1,text="Select Directory",command=fileselection).grid(row=0,column=0)
     
     mainwindow.mainloop()
