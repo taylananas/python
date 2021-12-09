527650a3eb6e6f40ce987c6c54eb1ad8f1e84086
Ryuusyr <shnitailan65@gmail.com>
2021-12-09 15:24:10 +0300

    09/12 son

--- menuproject/menuapp.py
+++ menuproject/menuapp.py
@@ -3,22 +3,32 @@ from tkinter import filedialog
 
 def main():
     mainwindow = Tk()
-    frame1 = LabelFrame(mainwindow, text="Ana")
-    frame1.grid(row=0,column=0)
+    frame1 = LabelFrame(mainwindow, text="Menü Seçimi")
+    frame1.grid(row=0,column=0,columnspan=3)
     def yenimenu():
         frame2 = LabelFrame(mainwindow, text="Menü")
-        frame2.grid(row=1,column=0)
+        frame2.grid(row=1,column=0,columnspan=3)
         menulist = selectedfile.readlines()
-        def ilerle1():
-            pass
         def siparis(isim):
             eksr = ekstratxt.get()
-            Button(frame3,text=f"{isim}\n{eksr}").pack()
+            siparisliste.insert(END,f"{isim}\n ----- {eksr}")
             ekstratxt.set("")
+        
+        def ilerle():
+            indx = siparisliste.curselection()
+            hazirlananliste.insert(END,siparisliste.get(indx))
+            siparisliste.delete(indx)
+
+        def ilerle2():
+            indx = hazirlananliste.curselection()
+            tamamlananliste.insert(END,hazirlananliste.get(indx))
+            hazirlananliste.delete(indx)
+
+
+
         a = 1
         b = 1
         c = 1
-        butonlar = []
         for i in menulist:
             btt = Button(frame2,text=f"Menü {c}: {i}", command= lambda i=i : siparis(i))
             btt.grid(row=a,column=b)
@@ -27,7 +37,6 @@ def main():
                 b+=1
                 a = 1
             c+=1
-            butonlar.append(btt)
         
         frame3 = LabelFrame(mainwindow, text="Siparişler")
         frame3.grid(row=3,column=0)
@@ -36,7 +45,30 @@ def main():
         frame5 = LabelFrame(mainwindow,text="Tamamlananlar")
         frame5.grid(row=3,column=2)
         frame6 = LabelFrame(mainwindow,text="İsim, Ekstralar")
-        frame6.grid(row=2,column=0)
+        frame6.grid(row=2,column=0,columnspan=3)
+
+        siparisliste = Listbox(frame3,width=50)
+        siparisliste.grid(row=0,column=0,rowspan=3)
+
+        rightbutton = Button(frame3, text ="--->",command=ilerle)
+        rightbutton.grid(row=1,column=2)
+
+        hazirlananliste = Listbox(frame4,width=50)
+        hazirlananliste.grid(row=0,column=0,rowspan=3)
+
+        rightbutton2 = Button(frame4, text ="--->",command=ilerle2)
+        rightbutton2.grid(row=1,column=2)
+
+        tamamlananliste = Listbox(frame5,width=50)
+        tamamlananliste.grid(row=0,column=0,rowspan=3)       
+        
+        def silmewk():
+            indx = tamamlananliste.curselection()
+            tamamlananliste.delete(indx)
+
+        silici = Button(frame5, text ="SİL",command=silmewk)
+        silici.grid(row=1,column=2) 
+
         ekstratxt = StringVar()
         ekstra = Entry(frame6, textvariable=ekstratxt)
         ekstra.grid(row=0,column=0)
@@ -53,9 +85,6 @@ def main():
     bt1.grid(row=0,column=0)
 
     mainwindow.mainloop()
-
-
-
 
 
 if __name__ == '__main__':
