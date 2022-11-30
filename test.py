ea5498f9cdb53fe371fdade7eeb3792e51525d0d
taylananas <shnitailan65@.com>
2022-11-30 12:30:15 +0300

    a

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello world")
+#points = [(x1,y1), (x2,y2)]
+def slope(points): #finds slope and y intercept
+    x1 = points[0][0]
+    y1 = points[0][1]
+    x2 = points[1][0]
+    y2 = points[1][1]
+
+    if x2-x1 > 0.0001:
+        slope = (y2-y1)/(x2-x1)
+        y_intercept = y1 - slope*x1
+        return slope,y_intercept
+    else:
+        return "Inf"
+print(slope([(1.9999,3),(2,4)]))
--- useful.py
+++ useful.py
@@ -1,10 +1,33 @@
-def quadratic(a,b,c):
-    kokler = ((-b + discriminant(a,b,c))/(2*a)), ((-b - discriminant(a,b,c))/(2*a))
+def quadratic(a,b,c): #finds roots of a quadratic equation
+    disc = discriminant(a,b,c)
+    kokler = ((-b + disc)/(2*a)), ((-b - disc)/(2*a))
     return kokler
 
-def discriminant(a,b,c):
+def discriminant(a,b,c): #finds discriminant of a quadratic equation
     dis = b**2 - 4*a*c
     return dis
 
-a, b, c= input("a, b, c: ").split()
-print(quadratic(float(a), float(b), float(c)))
+def isprime(a): #checks if a number is prime
+    if a < 2:
+        return False
+    for i in range(2, a):
+        if a % i == 0:
+            return False
+    return True
+
+def primefactors(a): #finds prime factors of a number
+    factors = []
+    for i in range(2, a):
+        if a % i == 0:
+            if isprime(i):
+                factors.append(i)
+    return factors
+
+def kineticenergy(m,v): #finds kinetic energy of a moving object
+    ke = 0.5*m*v**2
+    return ke
+
+def potentialenergy(m,g,h): #finds potential energy of an object
+    pe = m*g*h
+    return pe
+
